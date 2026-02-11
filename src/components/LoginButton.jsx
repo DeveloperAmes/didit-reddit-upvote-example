d49db4387c327a7001c5d615241fa167d041a528
AMY LOUISE TUCK <amyltuck@hotmail.com>
2026-02-11 14:10:16 +0000

    docs: adjust gitignore and login component

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,14 +1,18 @@
 import { signIn } from "@/auth";
 
-export function LoginButton() {
-  async function handleLogin() {
-    "use server";
-    await signIn();
-  }
-
+export function SignIn() {
   return (
-    <form action={handleLogin} className="inline">
-      <button className="bg-pink-300 text-black px-3 py-2 rounded">
+    <form
+      action={async () => {
+        "use server";
+        await signIn("github");
+      }}
+      className="inline"
+    >
+      <button
+        type="submit"
+        className="bg-pink-300 text-black px-3 py-2 rounded"
+      >
         Login
       </button>
     </form>
